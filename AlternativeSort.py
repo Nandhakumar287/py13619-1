d313599ec3e3a6f8db671a9b95decba92f8d7266
Nandha kumar S <mailstonandhakumar@gmail.com>
2019-06-13 12:25:09 +0530

    Update AlternativeSort.py

--- AlternativeSort.py
+++ AlternativeSort.py
@@ -1,9 +1,9 @@
 n=int(input())
 li=list(map(int,input().split()))[:n]
-li=li.sort()
 l=list()
 rev=-1
 forward=0
+li.sort()
 for i in range(0,n):
     if(i%2==0):
         l.append(li[rev])
@@ -11,4 +11,4 @@ for i in range(0,n):
     else:
         l.append(li[forward])
         forward+=1
-print(l)   
+print(l)     
